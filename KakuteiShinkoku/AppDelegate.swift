b4af51a42c5205b9d9cee1d3b5de29a5d9ebc9d5
Takuya Mukohira <takuya.mk96@gmail.com>
2018-01-24 14:10:01 +0900

    Initial Commit

---
+++ KakuteiShinkoku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KakuteiShinkoku
+//
+//  Created by Takuya Mukohira on 2018/01/24.
+//  Copyright © 2018年 Takuya Mukohira. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KakuteiShinkoku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KakuteiShinkoku
+//
+//  Created by Takuya Mukohira on 2018/01/24.
+//  Copyright © 2018年 Takuya Mukohira. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
